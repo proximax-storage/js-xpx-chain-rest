f3cd91d811993f5b021df2d8463b92ce30f08f5a
green <xgreenx9999@gmail.com>
2018-08-13 15:35:55 +0300

    Update mongodb install script

--- scripts/mongo/installMongoDb.sh
+++ scripts/mongo/installMongoDb.sh
@@ -1,5 +1,3 @@
-sudo su
-
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
 apt-get update
@@ -7,6 +5,7 @@ apt-get install -y mongodb-org --allow-unauthenticated
 
 cat ./scripts/mongo/mongodb.service > /etc/systemd/system/mongodb.service
 systemctl daemon-reload
-sudo systemctl start mongodb
-sudo systemctl status mongodb
-sudo systemctl enable mongodb
+systemctl start mongodb
+systemctl enable mongodb
+bash -c "/bin/bash ./scripts/mongo/mongoPrepare/mongors.sh"
+systemctl status mongodb
